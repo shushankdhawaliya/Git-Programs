c86236ff2efd72af0e389b214d9d60f89f74a1b1
Shushank Dhawaliya <shushankdhawaliya4941@gmail.com>
2026-01-09 23:19:07 +0530

    MaxProductSubArray

---
+++ MaxProductSubArray.java
@@ -0,0 +1,22 @@
+public class MaxProductSubArray {
+    public static void main(String[] args) {
+        int arr[] = {1,3,-5,6,4,0,3,9};
+        System.out.println(maxProduct(arr));
+    }
+     public static int maxProduct(int[] nums) {
+        int max = nums[0];
+        int min = nums[0];
+        int ans = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] < 0) {
+                int temp = max;
+                max = min;
+                min = temp;
+            }
+            max = Math.max(nums[i], max * nums[i]);
+            min = Math.min(nums[i], min * nums[i]);
+            ans = Math.max(ans, max);
+        }
+        return ans;
+    }
+} 
