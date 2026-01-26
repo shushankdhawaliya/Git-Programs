0caaa15580b9b17f43a36ad94a42f2461fa90f09
Shushank Dhawaliya <shushankdhawaliya4941@gmail.com>
2026-01-26 22:04:11 +0530

    One Extra line removed

--- SpiralOrder.java
+++ SpiralOrder.java
@@ -14,7 +14,7 @@ public class SpiralOrder {
         int colStart = 0;
         int colEnd = m-1;
         List <Integer> list = new ArrayList<>();
-        int num = 1;
+
         while(rowStart<=rowEnd && colStart<=colEnd){
             for(int i= colStart;i<=colEnd;i++){
                 list.add(matrix[colStart][i]);
